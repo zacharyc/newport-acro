ead101522392ce7c029692ce35fb304490ce9272
Zachary Cohen <zachary@zacharyc.com>
2023-04-22 16:54:08 -0400

    feat: adding a jams page and starting to clean up styles

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Bare v5.0.2 (https://startbootstrap.com/template/bare)
-* Copyright 2013-2022 Start Bootstrap
+* Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
 */
 // This file is intentionally blank
